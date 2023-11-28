7d07cdfce25786596e99edeba9bdf34f14eeda22
Simon Orupia <sorupia@gmail.com>
2023-11-28 23:02:46 +0300

    explore now design done

--- explore-now.php
+++ explore-now.php
@@ -1,22 +1,22 @@
-<div class="d-flex justify-content-center border border-1 rounded my-3 offset-md-2 col-md-8">
-    <form class="row row-cols-lg-auto g-4 py-3 mx-2">
-        <div class="col-12">
+<div class="d-flex justify-content-center border border-1 rounded my-3 offset-md-1 col-md-9">
+    <form class="row row-cols-md-auto g-4 py-2 mx-2">
+        <div class="col-12 border-right">
             <label class="label" for="search">Where you want to go</label>
             <input type="text" class="form-control" id="search" placeholder="Search your location">
         </div>
 
-        <div class="col-12">
+        <div class="col-12 border-right">
             <label class="label" for="checkin">Check-in</label>
             <input type="date" class="form-control" id="checkin" placeholder="Search your location">
         </div>
 
-        <div class="col-12">
+        <div class="col-12 border-right">
             <label class="label" for="checkout">Check-out</label>
             <input type="date" class="form-control" id="checkout" placeholder="Search your location">
         </div>
 
         <div class="col-12">
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary px-3 my-2" id="explore">Explore now</button>
         </div>
     </form>
 </div>
