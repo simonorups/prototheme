49c581aa02267e570fcb47ef9552e57e0f0b4ef4
Simon Orupia <sorupia@gmail.com>
2023-11-28 23:28:50 +0300

    final explore now

--- explore-now.php
+++ explore-now.php
@@ -1,4 +1,4 @@
-<div class="d-flex justify-content-center border border-1 rounded my-3 offset-md-2 col-md-8">
+<div class="d-flex justify-content-center border border-2 border-secondary rounded my-3 offset-md-2 col-md-8">
     <form class="row row-cols-md-auto g-4 py-2 mx-2">
         <div class="col-12 border-right">
             <label class="label" for="search">Where you want to go</label>
