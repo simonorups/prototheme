92fcaaa032755124c7f3e6541ea56e23ce612070
Simon Orupia <sorupia@gmail.com>
2023-12-02 10:33:31 +0300

    improved alignments

--- services.php
+++ services.php
@@ -7,10 +7,10 @@
             </div>
           </div> -->
         </div>
-        <div class="row text-left justify-content-between">
+        <div class="row text-left gx-5">
           <!-- Start Left services -->
-          <div class="col-md-2 col-sm-3 col-xs-12 shadow rounded">
-            <div class="about-move">
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="about-move shadow rounded p-3">
               <div class="services-details">
                 <div class="single-services">
                   <a class="services-icon" href="#">
@@ -26,8 +26,8 @@
               <!-- end about-details -->
             </div>
           </div>
-          <div class="col-md-2 col-sm-3 col-xs-12 shadow rounded">
-            <div class="about-move">
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="about-move shadow rounded p-3">
               <div class="services-details">
                 <div class="single-services">
                   <a class="services-icon" href="#">
@@ -43,9 +43,9 @@
               <!-- end about-details -->
             </div>
           </div>
-          <div class="col-md-2 col-sm-3 col-xs-12 shadow rounded">
+          <div class="col-md-3 col-sm-3 col-xs-12">
             <!-- end col-md-4 -->
-            <div class=" about-move">
+            <div class=" about-move shadow rounded p-3">
               <div class="services-details">
                 <div class="single-services">
                   <a class="services-icon" href="#">
@@ -61,9 +61,9 @@
               <!-- end about-details -->
             </div>
           </div>
-          <div class="col-md-2 col-sm-3 col-xs-12 shadow rounded">
+          <div class="col-md-3 col-sm-3 col-xs-12">
             <!-- end col-md-4 -->
-            <div class=" about-move">
+            <div class=" about-move shadow rounded p-3">
               <div class="services-details">
                 <div class="single-services">
                   <a class="services-icon" href="#">
