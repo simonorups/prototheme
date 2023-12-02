1cfd3c2b9f1ab66de290cbd7f672d1ed4ed51362
Simon Orupia <sorupia@gmail.com>
2023-12-02 11:28:00 +0300

    explore refined to its best

--- explore-now.php
+++ explore-now.php
@@ -1,22 +1,22 @@
-<div class="d-flex justify-content-center border border-2 border-secondary rounded my-3 offset-md-3 col-md-6">
-    <form class="row row-cols-md-auto g-4 py-2 mx-2">
-        <div class="col-12 border-right">
+<div class="container col-md-6 mt-5 mb-5">
+    <form class="row justify-content-md-center gx-4 py-2 border border-2 border-secondary rounded">
+        <div class="col-4 border-right">
             <label class="label" for="search">Where you want to go</label>
             <input type="text" class="form-control" id="search" placeholder="Search your location">
         </div>
 
-        <div class="col-12 border-right">
+        <div class="col-2 border-right">
             <label class="label" for="checkin">Check-in</label>
             <input type="date" class="form-control" id="checkin" placeholder="Search your location">
         </div>
 
-        <div class="col-12 border-right">
+        <div class="col-2">
             <label class="label" for="checkout">Check-out</label>
             <input type="date" class="form-control" id="checkout" placeholder="Search your location">
         </div>
 
-        <div class="col-12">
-            <button type="submit" class="btn btn-primary px-3 my-2" id="explore">Explore now</button>
+        <div class="col-4">
+            <button type="submit" class="btn btn-primary p-3 float-end" id="explore">Explore now</button>
         </div>
     </form>
 </div>
